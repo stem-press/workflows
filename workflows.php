e49353d7eaaedb0940524e9b79a4ebd8f679f483
Jon Gilkison <jon.gilkison@gmail.com>
2019-06-05 17:17:04 +0700

    Update composer

--- workflows.php
+++ workflows.php
@@ -4,7 +4,7 @@ Plugin Name: Stem Workflows
 Plugin URI: https://github.com/stem-press/workflows
 Description: Workflows for Stem
 Author: interfacelab
-Version: 0.2.0
+Version: 0.2.1
 Author URI: http://interfacelab.io
 */
 
